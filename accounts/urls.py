25582dba32c808e3b443c9f8a19256af51656fd9
Hemanth <hemanthvrm@gmail.com>
2018-03-14 03:20:45 -0400

    registration page changes

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    url(r'^login/', views.login, name='login'),
-    url(r'^signup/', views.signup, name='signup'),
+    url(r'^login/', views.loginpage, name='login'),
+    url(r'^signup/', views.signuppage, name='signup'),
 
 ]

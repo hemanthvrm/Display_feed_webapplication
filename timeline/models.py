be49b2b4640ac677930d96785c6ba52ab4659dc6
Hemanth <hemanthvrm@gmail.com>
2018-03-15 23:45:21 -0400

    Working on models

--- timeline/models.py
+++ timeline/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 
-# class post(models.Model):
+class post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    songname = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.songname

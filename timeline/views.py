269821a124916dfe69f51dec3b688ecbea01c5e6
Hemanth <hemanthvrm@gmail.com>
2018-03-20 08:10:32 -0400

    updates

--- timeline/views.py
+++ timeline/views.py
@@ -1,10 +1,33 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from . import models
+import requests
 
 
 def home(request):
     return render(request, "timeline/home.html")
 
 
+@login_required
 def post(request):
-    return render(request, "timeline/home.html")
-#     if request.method == "POST":
+    if request.method == "POST":
+        check_cached = ('user_info' in request.session)
+
+        if not check_cached:
+            ip_address = request.META.get('HTTP_X_FORWARDED_FOR', '')
+            resp = requests.get('http://freegeoip.net/json/%s' % ip_address)
+            request.session['user_info'] = resp.json()
+
+        user_info = request.session['user_info']
+
+        if request.POST["listeningpost"]:
+            post = models.Post()
+            post.author = request.user
+            post.songname = request.POST["listeningpost"]
+            post.created_date = timezone.datetime.now()
+            post.author_ipaddress = user_info['ip']
+            post.author_city = user_info['country_name']
+            post.save()
+
+    else:
+        return render(request, "timeline/home.html")

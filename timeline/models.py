6134160840b24b66871ea56994be81e6f4c9892c
Hemanth <hemanthvrm@gmail.com>
2018-03-20 08:11:39 -0400

    updates

--- timeline/models.py
+++ timeline/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
-class post(models.Model):
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+class Post(models.Model):
+    author = models.ForeignKey(User)
     songname = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
-
-    def __str__(self):
-        return self.songname
+    author_ipaddress = models.TextField()
+    author_city = models.TextField()

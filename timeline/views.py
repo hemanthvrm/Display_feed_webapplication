aee04b89832d15a38fb87b56100832edafb20426
Hemanth <hemanthvrm@gmail.com>
2018-04-02 23:31:53 -0500

    fixing post model issue

--- timeline/views.py
+++ timeline/views.py
@@ -13,28 +13,29 @@ def home(request):
     return render(request, "timeline/home.html", context)
 
 
-@login_required
-def post(request):
-    if request.method == "POST":
-        check_cached = ('user_info' in request.session)
+#@login_required
+def post_now(request):
+    return render(request, "timeline/home.html", context)
+    # if request.method == "POST":
+    #     check_cached = ('user_info' in request.session)
+    #     print("i am here")
+    #     if not check_cached:
+    #         ip_address = request.META.get('HTTP_X_FORWARDED_FOR', '')
+    #         resp = requests.get('http://freegeoip.net/json/%s' % ip_address)
+    #         request.session['user_info'] = resp.json()
 
-        if not check_cached:
-            ip_address = request.META.get('HTTP_X_FORWARDED_FOR', '')
-            resp = requests.get('http://freegeoip.net/json/%s' % ip_address)
-            request.session['user_info'] = resp.json()
+    #     user_info = request.session['user_info']
 
-        user_info = request.session['user_info']
-
-        if request.POST["listeningpost"]:
-            print("i am here")
-            post = Post()
-            post.author = request.user
-            post.songname = request.POST["listeningpost"]
-            post.created_date = timezone.datetime.now()
-            post.author_ipaddress = user_info['ip']
-            post.author_city = user_info['country_name']
-            print("i am here")
-            post.save()
+    #     if request.POST["listeningpost"]:
+    #         print("i am here")
+    #         post = Post()
+    #         post.author = request.user
+    #         post.songname = request.POST["listeningpost"]
+    #         post.created_date = timezone.datetime.now()
+    #         post.author_ipaddress = user_info['ip']
+    #         post.author_city = user_info['country_name']
+    #         print("i am here")
+    #         post.save()
 
     # else:
     #     return render(request, "timeline/home.html")

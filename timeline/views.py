c95a3bc6915662a1cd5fff97ed61bb5d3a4cac66
Hemanth <hemanthvrm@gmail.com>
2018-04-02 23:55:06 -0500

    bug fix

--- timeline/views.py
+++ timeline/views.py
@@ -1,41 +1,50 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.utils import timezone
 from .models import Post
 import requests
 
 
-def home(request):
-    tweet = Post.objects.all()
-    print(tweet)
-    context = {
-        'tweets': tweet
-    }
-    return render(request, "timeline/home.html", context)
+# def home(request):
+#     tweet = Post.objects.all()
+#     print(tweet)
+#     context = {
+#         'tweets': tweet
+#     }
+#     return render(request, "timeline/home.html", context)
 
 
-#@login_required
+@login_required
 def post_now(request):
-    return render(request, "timeline/home.html", context)
-    # if request.method == "POST":
-    #     check_cached = ('user_info' in request.session)
-    #     print("i am here")
-    #     if not check_cached:
-    #         ip_address = request.META.get('HTTP_X_FORWARDED_FOR', '')
-    #         resp = requests.get('http://freegeoip.net/json/%s' % ip_address)
-    #         request.session['user_info'] = resp.json()
 
-    #     user_info = request.session['user_info']
+    if request.method == "POST":
+        check_cached = ('user_info' in request.session)
+        if not check_cached:
+            ip_address = request.META.get('HTTP_X_FORWARDED_FOR', '')
+            resp = requests.get('http://freegeoip.net/json/%s' % ip_address)
+            request.session['user_info'] = resp.json()
 
-    #     if request.POST["listeningpost"]:
-    #         print("i am here")
-    #         post = Post()
-    #         post.author = request.user
-    #         post.songname = request.POST["listeningpost"]
-    #         post.created_date = timezone.datetime.now()
-    #         post.author_ipaddress = user_info['ip']
-    #         post.author_city = user_info['country_name']
-    #         print("i am here")
-    #         post.save()
+        user_info = request.session['user_info']
 
-    # else:
-    #     return render(request, "timeline/home.html")
+        if request.POST["listeningpost"]:
+            print("i am here")
+            post = Post()
+            post.author = request.user
+            post.songname = request.POST["listeningpost"]
+            # post.created_date = timezone.localtime()
+            post.author_ipaddress = user_info['ip']
+            post.author_city = user_info['country_name']
+            print("i am here")
+            post.save()
+            tweet = Post.objects.all()
+            context = {
+                'tweets': tweet
+            }
+            return render(request, "timeline/home.html", context)
+
+    else:
+        tweet = Post.objects.all()
+        context = {
+            'tweets': tweet
+        }
+        return render(request, "timeline/home.html", context)
